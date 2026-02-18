1864ad90237b05256f814e38be1e957156e655cf
Sarjna <sarjnapandya@gmail.com>
2026-02-18 10:43:51 +0530

    Initial project setup

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("❌ MongoDB Connection Error:");
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Secure Vault Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
